8109fb7e850a2f555fd8ff286a482bc17948436a
pratamamia_a_prihatmaja <pratamamia_a_prihatmaja@telkomsel.co.id>
2020-09-07 11:33:00 +0700

    define project skeleton

---
+++ stackoverflow_question/global.R
@@ -0,0 +1,12 @@
+# Load libraries
+library(flexdashboard)
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+library(scales)
+library(plotly)
+library(glue)
+
+# package shiny
+library(shiny)
+library(shinydashboard)
---
+++ stackoverflow_question/ui.R
@@ -0,0 +1,26 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+# Define UI for application that draws a histogram
+shinyUI(
+    dashboardPage(
+        
+        dashboardHeader(title = 'StackQuestion'),
+        dashboardSidebar(
+            
+            
+            
+        ),
+        dashboardBody(
+            
+            
+            
+        )
+    )    
+)
